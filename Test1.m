3a652aa1db644605bf1d280a7948b241e28c32eb
Karim Ahmadi Dastgerdi <kad62@brighton.ac.uk>
2025-02-15 20:16:55 +0000

    Second_edition

--- Test1.m
+++ Test1.m
@@ -3,12 +3,12 @@ clear
 close all
 %% defination equation dot(x)=a*x+b*u, u=sin(omega *t)
  % Developer name
-a=1; %param1
+a=-1; %param1
 b=2; %param2
 omega=0.2;% param3
 Ts=0.01;  % Sample time
 x(1)=0; %param 4 initial condition
-Tf=10; %final time
+Tf=20; %final time
 %% runing
 t=0:Ts:Tf;
 u=sin(t*omega);% input
@@ -19,8 +19,8 @@ x(i+1)=x(i)+Ts*(a*x(i)+b*u(i));
 i=i+1;
 end
 figure()
-gold on
+hold on
 grid minor
 plot(t,x,'b')
-plot(t,u,'t')
+plot(t,u,'r')
 
